0243fa9967aae6b9647e428f249c5f8c1cf90be4
Yue Wu <yuewu@outlook.com>
2014-01-05 22:57:26 +0800

    add exe_path.py

---
+++ exp/exe_path.py
@@ -0,0 +1,19 @@
+#!/usr/bin/evn python
+'''This file defines the path to executables, users may need to change the
+paths'''
+
+import platform
+
+#windows
+if platform.system() == "Windows":
+    SOL_exe_name = r'..\install\bin\SOL.exe'
+    vw_exe_name = 'vw'
+    liblinar_train_exe_name = r'..\extern\liblinear\train.exe'
+    liblinar_test_exe_name = r'..\extern\liblinear\train.exe'
+    analysis_exe_name = r'..\install\bin\analysis.exe'
+else:
+    SOL_exe_name = '../install/bin/SOL'
+    vw_exe_name = 'vw'
+    liblinar_train_exe_name = '../extern/liblinear/train'
+    liblinar_test_exe_name = '../extern/liblinear/test'
+    analysis_exe_name = '../install/bin/analysis'
