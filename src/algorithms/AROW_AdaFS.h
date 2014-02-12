394579f17d62590b698a13afede667eb97551d9a
Yue Wu <yuewu@outlook.com>
2014-02-12 14:00:28 +0800

    modify AROW_AdaFS.h

--- src/algorithms/AROW_AdaFS.h
+++ src/algorithms/AROW_AdaFS.h
@@ -25,22 +25,21 @@ namespace SOL {
 	protected:
 		float r;
 		s_array<float> sigma_w;
-		s_array<float> w_small;
-		s_array<float> w_large;
+		s_array<float> sigma_w_full;
 
-		HeapList<float> heap_small;
+		s_array<float> w_full;
+
 		HeapList<float> heap;
-		HeapList<float> heap_large;
 
 		IndexType K; //keep top K elemetns
-		size_t heap_gap_small; //length gap between heap_small and heap
-		size_t heap_gap_large; //length gap between heap and heap_large
+		size_t change_step; //change the update step
 
-		size_t errNum_small;
 		size_t errNum;
-		size_t errNum_large;
+		size_t errNum_full;
 
-		size_t delta_update_times;
+		size_t delta_update_num;
+
+		float tolerance;
 	public:
 		AROW_AdaFS(DataSet<FeatType, LabelType> &dataset,
 			LossFunction<FeatType, LabelType> &lossFunc);
@@ -69,7 +68,7 @@ namespace SOL {
 
 		//Change the dimension of weights
 		virtual void UpdateWeightSize(IndexType newDim);
-		int Update(const DataPoint<FeatType, LabelType> &x, s_array<float> &w, HeapList<float>& h, float &beta_t);
+		int Update_full(const DataPoint<FeatType, LabelType> &x);
 	};
 
 	template <typename FeatType, typename LabelType>
@@ -79,7 +78,8 @@ namespace SOL {
 			this->id_str = "AROW for Feature Selection";
 			this->r = init_r;
 			this->K = 0;
-			this->sigma_w.resize(this->weightDim);
+			this->w_full.resize(this->weightDim);
+			this->tolerance = 1.05;
 		}
 
 	template <typename FeatType, typename LabelType>
@@ -92,15 +92,16 @@ namespace SOL {
 	template <typename FeatType, typename LabelType>
 	float AROW_AdaFS<FeatType, LabelType>::UpdateWeightVec(
 		const DataPoint<FeatType, LabelType> &x) {
+		this->errNum_full += this->Update_full(x);
+
 		float y = this->Predict(x);
 		//y /= this->curIterNum;
 		float alpha_t = 1 - x.label * y;
-		float beta_t = 0;
 		if (alpha_t > 0){
 			IndexType index_i = 0;
 			size_t featDim = x.indexes.size();
 			//calculate beta_t
-			beta_t = this->r;
+			float beta_t = this->r;
 			for (size_t i = 0; i < featDim; i++){
 				beta_t += x.features[i] * x.features[i] * this->sigma_w[x.indexes[i]];
 			}
@@ -110,7 +111,8 @@ namespace SOL {
 				index_i = x.indexes[i];
 				if (this->heap.is_topK(index_i - 1)){
 					//update u_t
-					this->weightVec[index_i] += alpha_t * this->sigma_w[index_i] * x.label * x.features[i];
+					this->weightVec[index_i] += alpha_t *
+						this->sigma_w[index_i] * x.label * x.features[i];
 				}
 				else{
 					this->weightVec[index_i] = 0;
@@ -127,56 +129,39 @@ namespace SOL {
 			this->weightVec[0] += alpha_t * this->sigma_w[0] * x.label;
 			//this->sigma_w[0] -= beta_t * this->sigma_w[0] * this->sigma_w[0];
 			this->sigma_w[0] *= this->r / (this->r + this->sigma_w[0]);
+
 			this->errNum++;
 		}
+		this->delta_update_num++;
+		//if (this->delta_update_num > this->change_step){
+		if (this->delta_update_num > 20000){
+			IndexType newsize = 0;
+			if (this->errNum < this->errNum_full * this->tolerance){
+				newsize = this->heap.GetK() / 2;
+				this->change_step = newsize;
+				newsize = newsize > 0 ? newsize : 1;
+			}
+			else{
+				newsize = (this->heap.GetK() + this->weightDim - 1) / 2;
+				this->change_step = this->weightDim - 1 - newsize;
+			}
+			//copy sigma and w
+			memcpy(this->weightVec.begin, this->w_full.begin, sizeof(float)* this->w_full.size());
+			memcpy(this->sigma_w.begin, this->sigma_w_full.begin, sizeof(float)* this->sigma_w_full.size());
+			this->heap.ResizeHeap(newsize);
+			this->errNum = this->errNum_full = 0;
+			this->delta_update_num = 0;
+		}
 
-		this->errNum_small += this->Update(x,this->w_small,this->heap_small,beta_t);
-		this->errNum_large += this->Update(x,this->w_large,this->heap_large,beta_t);
-		this->delta_update_times++;
-		if (this->delta_update_times > this->heap_gap_small){
-			if (this->errNum_small < this->errNum * 1.01){ //shrink the budget size
-				IndexType large_size = this->heap.GetK();
-				IndexType cur_size = this->heap_small.GetK();
-				this->heap_gap_large = large_size - cur_size;
-				this->heap_gap_small *= 2;
-				this->heap_gap_small += 1;
-				IndexType small_size = cur_size - this->heap_gap_small;
-				if (small_size < 1){
-					small_size = 1;
-					this->heap_gap_small = cur_size -small_size;
-				}
-				this->heap_large.ResizeHeap(large_size);
-				this->heap.ResizeHeap(cur_size);
-				this->heap.ResizeHeap(small_size);
-			}
-		}
-		if (this->delta_update_times > this->heap_gap_large){
-			if (this->errNum > this->errNum_large * 1.01){//enlarge the budget size
-				IndexType small_size = this->heap.GetK();
-				IndexType cur_size = this->heap_large.GetK();
-				this->heap_gap_small = cur_size - small_size;
-				this->heap_gap_large *= 2;
-				this->heap_gap_large += 1;
-				IndexType large_size = cur_size + this->heap_gap_large;
-				if (large_size >= this->weightDim){
-					large_size = this->weightDim - 1;
-					this->heap_gap_large = this->weightDim - 1;
-				}
-				this->heap_small.ResizeHeap(small_size);
-				this->heap.ResizeHeap(cur_size);
-				this->heap_large.ResizeHeap(large_size);
-			}
-		}
 		return y;
 	}
 
 	template <typename FeatType, typename LabelType>
-	int AROW_AdaFS<FeatType, LabelType>::Update(const DataPoint<FeatType, LabelType> &x,
-		s_array<float> &w, HeapList<float>& h, float &beta_t){
-		float y = w[0];
+	int AROW_AdaFS<FeatType, LabelType>::Update_full(const DataPoint<FeatType, LabelType> &x){
+		float y = this->w_full[0];
 		size_t featDim = x.indexes.size();
 		for (size_t i = 0; i < featDim; i++){
-			y += w[x.indexes[i]] * x.features[i];
+			y += this->w_full[x.indexes[i]] * x.features[i];
 		}
 
 		float alpha_t = 1 - x.label * y;
@@ -184,33 +169,21 @@ namespace SOL {
 		IndexType index_i = 0;
 		if (alpha_t > 0){
 			//calculate beta_t
-			if (beta_t == 0){
-				beta_t = this->r;
-				for (size_t i = 0; i < featDim; i++){
-					beta_t += x.features[i] * x.features[i] * this->s[x.indexes[i]];
-				}
-				beta_t = 1.f / beta_t;
-			}
+			float beta_t = this->r;
+			for (size_t i = 0; i < featDim; i++)
+				beta_t += x.features[i] * x.features[i] * this->sigma_w_full[x.indexes[i]];
+			beta_t = 1.f / beta_t;
 			alpha_t *= beta_t;
 			for (size_t i = 0; i < featDim; i++){
 				index_i = x.indexes[i];
-				if (h.is_topK(index_i - 1)){
-					//update u_t
-					this->w[index_i] += alpha_t * this->s[index_i] * x.label * x.features[i];
-				}
-				else{
-					this->w[index_i] = 0;
-				}
+				this->w_full[index_i] += alpha_t * this->sigma_w_full[index_i] * x.label * x.features[i];
 				//update sigma_w
-				this->s[index_i] *= this->r / (this->r +
-					this->s[index_i] * x.features[i] * x.features[i]);
-				IndexType ret_id;
-				this->h.UpdateHeap(index_i - 1, ret_id);
+				this->sigma_w_full[index_i] *= this->r / (this->r +
+					this->sigma_w_full[index_i] * x.features[i] * x.features[i]);
 			}
 			//bias term
-			this->w[0] += alpha_t * this->s[0] * x.label;
-			this->s[0] *= this->r / (this->r + this->s[0]);
-			
+			this->w_full[0] += alpha_t * this->sigma_w_full[0] * x.label;
+			this->sigma_w_full[0] *= this->r / (this->r + this->sigma_w_full[0]);
 			return 1;
 		}
 		return 0;
@@ -225,21 +198,17 @@ namespace SOL {
 			cerr << "current number: " << this->K << endl;
 			exit(0);
 		}
-		if (this->weightDim < this->K + 1){
-			this->UpdateWeightSize(this->K); //remove the bais term
+		if (this->weightDim < 2 * this->K + 1){
+			this->UpdateWeightSize(2 * this->K); //remove the bais term
 		}
 		this->sigma_w.set_value(1);
+		this->sigma_w_full.set_value(1);
 		heap.Init(this->weightDim - 1, this->K, this->sigma_w.begin + 1);
-		heap_small.Init(this->weightDim - 1, this->K, this->sigma_w.begin + 1);
-		heap_large.Init(this->weightDim - 1, this->K, this->sigma_w.begin + 1);
-		this->heap_gap_large = 0;
-		this->heap_gap_small = 0;
-		this->w_large.zeros();
-		this->w_small.zeros();
-		this->errNum_large = 0;
+		this->w_full.zeros();
 		this->errNum = 0;
-		this->errNum_small = 0;
-		this->delta_update_times = 0;
+		this->errNum_full = 0;
+		this->delta_update_num = 0;
+		this->change_step = this->K;
 		//this->is_normalize = true; //AROW requires normalization
 	}
 
@@ -247,6 +216,7 @@ namespace SOL {
 	template <typename FeatType, typename LabelType>
 	void AROW_AdaFS<FeatType, LabelType>::EndTrain() {
 		Optimizer<FeatType, LabelType>::EndTrain();
+		cout << "learned buffer size: " << this->heap.GetK() << endl;
 	}
 
 	template <typename FeatType, typename LabelType>
@@ -273,21 +243,19 @@ namespace SOL {
 			this->sigma_w.set_value(this->sigma_w.begin + this->weightDim,
 				this->sigma_w.end, 1);
 
+			this->sigma_w_full.reserve(newDim + 1);
+			this->sigma_w_full.resize(newDim + 1);  //reserve the 0-th
+			//set the rest to 1
+			this->sigma_w_full.set_value(this->sigma_w_full.begin + this->weightDim,
+				this->sigma_w_full.end, 1);
+
 			heap.UpdateDataNum(newDim, this->sigma_w.begin + 1);
-			heap_small.UpdateDataNum(newDim, this->sigma_w.begin + 1);
-			heap_large.UpdateDataNum(newDim, this->sigma_w.begin + 1);
 
-			this->w_small.reserve(newDim + 1);
-			this->w_small.resize(newDim + 1);
+			this->w_full.reserve(newDim + 1);
+			this->w_full.resize(newDim + 1);
 			//set the new value to zero
-			this->w_small.zeros(this->w_small.begin + this->weightDim,
-				this->w_small.end);
-
-			this->w_large.reserve(newDim + 1);
-			this->w_large.resize(newDim + 1);
-			//set the new value to zero
-			this->w_large.zeros(this->w_large.begin + this->weightDim,
-				this->w_large.end);
+			this->w_full.zeros(this->w_full.begin + this->weightDim,
+				this->w_full.end);
 
 			Optimizer<FeatType, LabelType>::UpdateWeightSize(newDim);
 		}
